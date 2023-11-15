029b4d0d7acd65539d6f0f18921d3279782c7c50
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2023-11-15 16:10:04 -0400

    feat: grid 6 x 6

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -19,4 +19,74 @@ export const cards = [
     back: "🍺",
     flipped: false,
   },
+  {
+    id: "5",
+    back: "🍣",
+    flipped: false,
+  },
+  {
+    id: "6",
+    back: "🌭",
+    flipped: false,
+  },
+  {
+    id: "7",
+    back: "🍟",
+    flipped: false,
+  },
+  {
+    id: "8",
+    back: "🥗",
+    flipped: false,
+  },
+  {
+    id: "9",
+    back: "🍓",
+    flipped: false,
+  },
+  {
+    id: "10",
+    back: "🍊",
+    flipped: false,
+  },
+  {
+    id: "11",
+    back: "🍰",
+    flipped: false,
+  },
+  {
+    id: "12",
+    back: "🍕",
+    flipped: false,
+  },
+  {
+    id: "13",
+    back: "🍔",
+    flipped: false,
+  },
+  {
+    id: "14",
+    back: "🍤",
+    flipped: false,
+  },
+  {
+    id: "15",
+    back: "🍜",
+    flipped: false,
+  },
+  {
+    id: "16",
+    back: "🍙",
+    flipped: false,
+  },
+  {
+    id: "17",
+    back: "☕",
+    flipped: false,
+  },
+  {
+    id: "18",
+    back: "🍵",
+    flipped: false,
+  },
 ];
